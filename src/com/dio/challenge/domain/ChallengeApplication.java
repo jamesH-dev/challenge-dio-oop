968dbbd7c248312f4a5a2bdc6d1fb0f3f09cb631
jamesH-dev <jamesrpm@hotmail.com>
2021-11-30 22:47:43 -0300

    Abstração e Encapsulamento

---
+++ src/com/dio/challenge/domain/ChallengeApplication.java
@@ -0,0 +1,21 @@
+package com.dio.challenge.domain;
+
+import java.time.LocalDate;
+
+public class ChallengeApplication {
+    public static void main(String[] args) {
+        Course course = new Course();
+
+        course.setTitle("Entendendo POO");
+        course.setDescription("Neste curso, você irá aprender tudo sobre esse paradigma!");
+        course.setWorkload(2);
+
+        Mentoring mentoring = new Mentoring();
+        mentoring.setTitle("Abstração e Encapsulamento");
+        mentoring.setDescription("Nesta mentoria, você aprenderá seus conceitos e também colocará em prática o conteúdo estudado.");
+        mentoring.setDate(LocalDate.now());
+
+        System.out.println(course);
+        System.out.println(mentoring);
+    }
+}
